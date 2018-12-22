48121807968ee343f47b351f42cfa1f297e5aa88
srimani-programmer <srimani.scholar@gmail.com>
2018-12-22 09:47:16 +0530

    Updating the repository.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Skill Development
-This is basically a Hackathon project in final round. which is done by associating with Telangana government. Source code for this project.
--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'mydb');
-define('DB_PASSWORD','');
-define('DB_DATABASE','skilldevelopment');
-$db = mydqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
