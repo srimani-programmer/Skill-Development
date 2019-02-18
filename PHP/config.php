4858dc4c66de3c6ed9164c1017381bfbeccf7cc8
Sai Vardhan Poloju <43542979+vardhan-scripter@users.noreply.github.com>
2019-02-18 17:15:09 +0530

    config file updated

---
+++ PHP/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'mydb');
+define('DB_PASSWORD','');
+define('DB_DATABASE','skilldevelopment');
+$db = mydqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
